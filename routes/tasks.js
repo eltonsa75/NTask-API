3242f78c95c793ee4f8f518146b04cf0adfbb591
Elton Andrade <eltonsa75@hotmail.com>
2019-11-26 17:57:28 -0300

    Organizando rotas das Tarefas

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,42 @@
 module.exports = app => {
     const Tasks = app.db.models.Tasks;
-    app.get("/tasks", (req, res) => {
-        Tasks.findAll({}).then(tasks => {
-            res.json({tasks: tasks});
+
+    app.route("/tasks")
+        .all((req, res, next) => {
+        // Middleware de pré-execução das rotas
+        delete req.body.id;
+        next();
+    })
+        .get((req, res, next) => {
+        // "/tasks": Lista tarefas
+        delete req.body.id;
+        next();
+    })
+        .post((req, res, next) => {
+            // "/tasks": Cadastra uma nova tarefa
+            delete req.body.id;
+            next();
         });
-    });
+    app.route("/tasks/:id")
+        .all((req, res, next) => {
+            // Middleware de pré-execução das rotas
+            delete req.body.id;
+            next();
+        })
+        .get((req, res, next) => {
+            // "/tasks/1": consulta uma tarefa  
+            delete req.body.id;
+            next();
+        })
+        .put((req, res, next) => {
+            // "/tasks/1": Atualiza uma tarefa
+            delete req.body.id;
+            next();
+        })
+        .delete((req, res, next) => {
+            // "/tasks/1": Excluir uma tarefa
+            delete req.body.id
+            next();
+        });
 };
+
