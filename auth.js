646a3665fa6cfc0a12faadeb01da2cab5d1be3a6
Elton Andrade <eltonsa75@hotmail.com>
2019-11-28 01:08:41 -0300

    Implementação do back-end da aplicação

---
+++ auth.js
@@ -0,0 +1,31 @@
+import passport from "passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+
+
+module.exports = app => {
+    const Users = app.db.models.Users;
+    const cfg = app.libs.config;
+    const params = {
+        secretOrkey: cfg.jwtSecret,
+        jwtFromrequest: ExtractJwt.fromAuthHeader()
+    };
+    const Strategy = new Strategy(params, (payload, done) => {
+        Users.findById(payload.id)
+            .then(user => {
+                if (user) {
+                    return done(null, {
+                        id: user.id,
+                        email: user.email
+                    });
+                }
+                return done(null, false);
+            })
+            .catch(error => done(error, null));
+    });
+    passport.use(Strategy);
+    return {
+        initialize: () => {
+            return passport.authenticate("jwt", cfg.jwtSession);
+        }
+    };
+};
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,51 +2,63 @@ module.exports = app => {
     const Tasks = app.db.models.Tasks;
 
     app.route("/tasks")
+    all(app.auth.authenticate())
         .get((req, res) => {
-            Tasks.findAll({})
-            .then(result => res.json(result))
-            .catch(error => {
-                res.status(412).json({msg: error.message});
-            });
-    })
-        .post((req, res) => {
-            Tasks.create(req.body)
-            .then(result => res.json(result))
-            .catch(error => {
-                res.status(412).json({msg: error.message});
-            });
-        })
-    app.route("/tasks/:id")
-        .all((req, res, next) => {
-            delete req.body.id;
-            next();
-        })
-        .get((req, res, next) => {
-            Tasks.findOne({where: req.params})
-            .then(result => {
-                if (result) {
-                    res.json(result);
-                } else {
-                    res.sendStatus(404);
-                }
+            Tasks.findAll({
+                where: { user_id: req.user.id}
             })
-            .catch(error => {
-                res.status(412).json({msg: error.message});
-            });
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
+        })
+        .post((req, res) => {
+            req.body.user_id = req.user.id;
+            Tasks.create(req.body)
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
+        });
+
+    app.route("/tasks/:id")
+    all(app.auth.authenticate())
+        .get((req, res) => {
+            Tasks.findOne({ where: {
+                id: req.params.id,
+                user_id: req.user.id
+                }})
+                .then(result => {
+                  if (result) {
+                    return res.json(result);
+                }
+                return res.sendStatus(404);
+                })
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
         })
         .put((req, res) => {
-            Tasks.update(req.body, {where: req.params})
-            .then(result => res.sendStatus(204))
-            .catch(error => {
-                res.status(412).json({msg: error.message});
-            });           
+            Tasks.update(req.body, {
+                where: {
+                id: req.params.id,
+                user_id: req.user.id
+                }})
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                res.status(412).json({ msg: error.message });
+                });
         })
-        .delete((req, res, next) => {
-           Tasks.destroy({where: req.params})
-           .then(result => res.sendStatus(204))
-           .catch(error => {
-               res.status(412).json({msg: error.message});
-           });
+        .delete((req, res) => {
+            Tasks.destroy({
+                where: {
+                    id: req.params.id,
+                    user_id: req.user.id
+                }})
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({ msg: error.message });
+                });
         });
 };
 
